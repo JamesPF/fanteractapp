0f1f36efc4276ed12796b4f45d427a8cd9d5b2f5
JamesPF <jamesfelz@gmail.com>
2014-12-16 11:16:10 -0800

    search fully functional

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,8 +2,18 @@ class ResultsController < ApplicationController
 
   def index
     @results = Experience.all
-    @results = @results.where("lower(title) LIKE ?", "%#{params[:title].downcase}%") if !params[:title].blank?
-    # @results = @results.joins(:user)
+    @results = @results.joins(:user)
+    @results = Experience.find_by_sql([
+      'SELECT experiences.id, experiences.title, users.artist_name, experiences.description, experiences.amount_available ' +
+      'FROM experiences ' +
+      'INNER JOIN users ' +
+      'ON experiences.user_id = users.id ' +
+      'WHERE lower(users.artist_name) LIKE ? OR lower(experiences.title) LIKE ?',
+      "%#{params[:title].downcase}%", "%#{params[:title].downcase}%"
+      ])
+
+
+
   end
 
   def create
