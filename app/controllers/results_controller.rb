82c89de745609b9fe26dea478fc7b6333b055cf5
JamesPF <jamesfelz@gmail.com>
2014-12-16 10:26:25 -0800

    search results now display artist name

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,7 +4,6 @@ class ResultsController < ApplicationController
     @results = Experience.all
     @results = @results.where("lower(title) LIKE ?", "%#{params[:title].downcase}%") if !params[:title].blank?
     # @results = @results.joins(:user)
-    # @results = @results.where(:users => {:zip => params[:zip]}) if !params[:zip].blank?
   end
 
   def create
