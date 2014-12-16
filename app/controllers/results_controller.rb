d5ec87bf50701c40d33f0323512e4476a5b5c41b
JamesPF <jamesfelz@gmail.com>
2014-12-15 17:01:27 -0800

    all experiences now show up in search results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,16 @@
 class ResultsController < ApplicationController
+
+  def index
+    @results = Experience.all
+    @results = @results.where("lower(name) LIKE ?", "%#{params[:user_id]}%") if !params[:user_id].blank?
+    # @results = @results.joins(:user)
+    # @results = @results.where(:users => {:zip => params[:zip]}) if !params[:zip].blank?
+  end
+
+  def create
+  end
+
+  def show
+  end
+
 end
