8fd02e35847b0be56a43d9d88fa1ac4c70ff5b25
JamesPF <jamesfelz@gmail.com>
2014-12-15 17:29:13 -0800

    minor changes from last push

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,7 +2,7 @@ class ResultsController < ApplicationController
 
   def index
     @results = Experience.all
-    @results = @results.where("lower(name) LIKE ?", "%#{params[:user_id]}%") if !params[:user_id].blank?
+    @results = @results.where("lower(title) LIKE ?", "%#{params[:title].downcase}%") if !params[:title].blank?
     # @results = @results.joins(:user)
     # @results = @results.where(:users => {:zip => params[:zip]}) if !params[:zip].blank?
   end
