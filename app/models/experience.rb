6b3dce20a66778fd4bccf59abbc271a9bdcbcd19
JamesPF <jamesfelz@gmail.com>
2014-12-17 10:52:08 -0800

    required fields on experience new

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,11 @@
 class Experience < ActiveRecord::Base
 
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :amount_available
+  validates_presence_of :availability
+  validates_presence_of :description
+
   belongs_to :user
   has_many :orders
 
